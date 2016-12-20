dbcce9cc80283f8888c5d98ea27527f2eadf29a1
limingyang <1597152697@qq.com>
2016-12-20 11:19:13 +0800

    mod

--- index.php
+++ index.php
@@ -121,34 +121,6 @@ $signPackage = $jssdk->GetSignPackage();
         'openCard'
       ]
   });
-
-var latitude = 0,
-	longitude = 0;
-document.getElementById("where").onclick=function(){
-	wx.getLocation({
-	    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
-	    success: function (res) {
-	        latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-	        longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-	        var speed = res.speed; // 速度，以米/每秒计
-	        var accuracy = res.accuracy; // 位置精度
-	    }
-	});
-	alert(accuracy);
-};
-document.getElementById("where2").onclick=function(){
-	wx.openLocation({
-	    latitude: latitude, // 纬度，浮点数，范围为90 ~ -90
-	    longitude: longitude, // 经度，浮点数，范围为180 ~ -180。
-	    name: '', // 位置名
-	    address: '', // 地址详情说明
-	    scale: 1, // 地图缩放级别,整形值,范围从1~28。默认为最大
-	    infoUrl: '' // 在查看位置界面底部显示的超链接,可点击跳转
-	});
-	alert(name+","+address);
-}
-
-	
 </script>
 <script type="text/javascript" src="template/baiduTemplate.js"></script>
 <div id="mymoban"></div>
