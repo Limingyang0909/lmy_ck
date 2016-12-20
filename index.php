47c3cb62aeda789dc29cbea968ba18bab402c507
limingyang <1597152697@qq.com>
2016-12-20 10:39:09 +0800

    mog

--- index.php
+++ index.php
@@ -127,16 +127,16 @@ $signPackage = $jssdk->GetSignPackage();
 	var latitude = 0,
 		longitude = 0;
 document.getElementById("where").onclick=function(){
-		wx.getLocation({
+	wx.getLocation({
 	    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
 	    success: function (res) {
 	        latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
 	        longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
 	        var speed = res.speed; // 速度，以米/每秒计
 	        var accuracy = res.accuracy; // 位置精度
-	        alert(latitude+","+longitude)
 	    }
 	});
+	alert(latitude+","+longitude);
 };
 document.getElementById("where2").onclick=function(){
 	wx.openLocation({
